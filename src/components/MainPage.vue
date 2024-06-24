8cdade280f82eb0a0cd8d10862bcc4a888fe1b5e
RomanBeldent <romanbeldent@gmail.com>
2024-06-24 12:19:56 +0200

    vue3 bootstrap installed

---
+++ src/components/MainPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Elemate Pet Finder</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'MainPage'
+});
+</script>
+
+<style scoped></style>
