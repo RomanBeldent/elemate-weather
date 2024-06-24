9f8e2a09acc3f4d34cb4634d0576a33ec89ee9e0
RomanBeldent <romanbeldent@gmail.com>
2024-06-24 14:08:43 +0200

    axios installed

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@ import App from './App.vue'
 import BootstrapVue3 from 'bootstrap-vue-3';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 const app = createApp(App);
 
+app.use(VueAxios, axios)
 app.use(BootstrapVue3);
 app.mount('#app')
